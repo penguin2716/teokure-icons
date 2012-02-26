29c222b58cb0da1ec8887d5c3a4d2e77a804756c
Takuma Nakajima <penguin.trance.2716@gmail.com>
2012-02-27 04:39:53 +0900

    add number of icon types to title

--- tohtml.sh
+++ tohtml.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 htmlfile="index.html"
-
+total=`ls img/* | wc -l`
+title="ておくれアイコンずかん（全${total}種類）"
 echo "<html>" > ${htmlfile}
-echo "<head><title>ておくれアイコンずかん</title></head>" >> ${htmlfile}
+echo "<head><title>${title}</title></head>" >> ${htmlfile}
 echo "<body>" >> ${htmlfile}
-echo "<h1>ておくれアイコンずかん</h1>" >> ${htmlfile}
+echo "<h1>${title}</h1>" >> ${htmlfile}
 for img in `ls img/*`
 do
     echo "<img src=\"${img}\" height=130 border=0 title=${img}>" >> ${htmlfile}
